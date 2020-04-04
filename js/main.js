98234f7d534a40b3adb19ab936b471bf5e7db18c
Jake Mattless <416699@edu.cwa.ac.uk>
2020-04-04 12:04:56 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,69 @@
+let navbar = document.getElementById('NavBar');
+let scrollBox = document.getElementById('scrollBox')
+let originalWidth = navbar.style.width;
+let originalLeft = navbar.style.left;
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        navbar.style.width = "100%";
+        navbar.style.left = "0";
+        navbar.style.borderRadius = "0";
+        document.getElementById('Lbd').style.borderRadius = "0";
+        document.getElementById('Rbd').style.borderRadius = "0";
+    }
+    else {
+        navbar.style.width = originalWidth;
+        navbar.style.left = originalLeft;
+        navbar.style.borderRadius = "0 0 50px 50px";
+        document.getElementById('Lbd').style.borderRadius = "0 0 0 50px";
+        document.getElementById('Rbd').style.borderRadius = "0 0 50px 0";
+    }
+
+    if (document.documentElement.scrollTop >= document.getElementById('PartFive').offsetTop-80) {
+        document.getElementById('Lbd').classList.remove('active');
+        document.getElementById('SecondLink').classList.remove('active');
+        document.getElementById('ThirdLink').classList.remove('active');
+        document.getElementById('FourthLink').classList.remove('active');
+        document.getElementById('Rbd').classList.add('active');
+    }
+    else if(document.documentElement.scrollTop >= document.getElementById('PartThree').offsetTop-80) {
+        document.getElementById('Lbd').classList.remove('active');
+        document.getElementById('SecondLink').classList.remove('active');
+        document.getElementById('ThirdLink').classList.add('active');
+        document.getElementById('FourthLink').classList.remove('active');
+        document.getElementById('Rbd').classList.remove('active');
+    }
+    else if(document.documentElement.scrollTop >= document.getElementById('PartTwo').offsetTop-80) {
+        document.getElementById('Lbd').classList.remove('active');
+        document.getElementById('SecondLink').classList.add('active');
+        document.getElementById('ThirdLink').classList.remove('active');
+        document.getElementById('FourthLink').classList.remove('active');
+        document.getElementById('Rbd').classList.remove('active');
+    }
+    else {
+        document.getElementById('Lbd').classList.add('active');        
+        document.getElementById('SecondLink').classList.remove('active');
+        document.getElementById('ThirdLink').classList.remove('active');
+        document.getElementById('FourthLink').classList.remove('active');
+        document.getElementById('Rbd').classList.remove('active');
+    }
+}
+
+let blogContainer = document.getElementById('BlogContainer');
+let temp = document.getElementById('Temp');
+
+window.onload = function() {
+    let txtFile = new XMLHttpRequest();
+    // txtFile.open("GET", "https://tempsiteforzin.netlify.com/BlogPosts/TestDoc.txt", true);
+    txtFile.onreadystatechange = function() {
+        if (txtFile.readyState === 4) {
+            if (txtFile.status === 200 || txtFile.status == 0) {
+                let allText = txtFile.responseText;
+                let lines = txtFile.responseText.split("\n")
+                console.log(allText);
+                temp.innerHTML = allText;
+            }
+        }
+    }
+    txtFile.send(null);
+}
